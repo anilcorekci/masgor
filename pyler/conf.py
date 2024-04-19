1f630c6482be3209b10ccd6c59abddc2123bbbd8
anilcorekci <35172975+anilcorekci@users.noreply.github.com>
2024-04-19 07:38:59 +0300

    Update conf.py

--- pyler/conf.py
+++ pyler/conf.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
 #! coding:utf-8 -*-
 
-import ConfigParser
-class config(ConfigParser.RawConfigParser):
-        def __init__(self,confile):
-                ConfigParser.RawConfigParser.__init__(self)
-                self.path = confile
-                self.readfp(open(self.path))
+import configparser
 
-        def yaz(self,file): 
-                with open(self.path, 'wb') as configfile:
-                    self.write(configfile)
-        def set_conf(self,file,key,name): 
-                try:
-                        self.add_section(file)
-                except:pass       
-                if self._get(file,key) == name:
-                        return 
-                self.set(file,key,name) 
-                self.yaz(file)     
-        def _get(self,file,key):
-                try:
-                        val = self.get(file,key)
-                except:
-                        return "Bilinmiyor"
-                else:return val        
+class config(configparser.RawConfigParser):
+    def __init__(self,confile):
+        configparser.RawConfigParser.__init__(self)
+        self.path = confile
+        self.readfp(open(self.path))
 
-                        
+    @property
+    def dict_(self):
+        return self._sections['tercihler.cfg']
+
+    def yaz(self,file): 
+        with open(self.path, 'w') as configfile:
+            self.write(configfile)
+
+    def set_conf(self,file,key,name): 
+        try:
+            self.add_section(file)
+        except:pass
+        if self._get(file,key) == name:
+            return 
+        self.set(file,key,name)
+        self.yaz(file)
+
+    def _get(self,file,key):
+        try:
+            val = self.get(file,key)
+        except:
+            return False
+        else:return val
