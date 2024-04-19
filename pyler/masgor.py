d5d1175a6dd8866312016d6fe0f1c56bd3aa7be2
anilcorekci <35172975+anilcorekci@users.noreply.github.com>
2024-04-19 07:37:27 +0300

    Update masgor.py

--- pyler/masgor.py
+++ pyler/masgor.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 #! coding:utf-8 -*-
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk as gtk
+from gi.repository import Gdk as gdk 
 from gi.repository import Gtk as gtk
 from gi.repository import GdkPixbuf as gdkpixbuf
 import os 
 USER = os.environ["HOME"].strip("\n")
+
+import tercihler
+from hakkinda import hakkinda
+
  
 class area(object):
     def __init__(self):
@@ -12,48 +20,44 @@ class area(object):
         self.statusIcon.set_from_pixbuf(pix)
         self.statusIcon.set_visible(True)
         self.statusIcon.set_tooltip_text("MASGOR")
+      #  print(dir(self.statusIcon))
 
-	self.menu = gtk.Menu()
+        self.menu = gtk.Menu()
         
-	self.Item = gtk.CheckMenuItem("Başlat",None)
-        self.Item.connect('activate', self.komut, self.statusIcon)           
+        self.Item = gtk.CheckMenuItem(label="Başlat")
+        self.Item.connect('activate',
+            lambda *x:
+                os.system("./masgor 1 &") if self.Item.get_active()\
+                else os.system("killall masgor &"))                           
+        
         self.menu.append(self.Item)
         
-	self.Item1 = gtk.ImageMenuItem(gtk.STOCK_PREFERENCES)
-	self.Item1.connect('activate', self.tercihler, self.statusIcon)           
+        self.Item1 = gtk.ImageMenuItem(label=gtk.STOCK_PREFERENCES)
+        self.Item1.connect('activate', 
+                lambda *x: tercihler.ayar() )           
         self.menu.append(self.Item1)
         
-        self.Item3 = gtk.ImageMenuItem(gtk.STOCK_ABOUT)
-	self.Item3.connect('activate', self.komut2, self.statusIcon)           
+        self.Item3 = gtk.ImageMenuItem(label=gtk.STOCK_ABOUT)
+        self.Item3.connect('activate',
+                lambda *x: hakkinda() )           
         self.menu.append(self.Item3)
         
-        self.Item2 = gtk.ImageMenuItem(gtk.STOCK_QUIT)
-        self.Item2.connect('activate', self.komut1, self.statusIcon)           
+        self.Item2 = gtk.ImageMenuItem(label=gtk.STOCK_QUIT)
+        self.Item2.connect('activate',
+                lambda *x: [os.system("killall masgor &"),\
+                gtk.main_quit() ])   
+          
         self.menu.append(self.Item2)
  
- 
-
         self.menu.show_all()
+
         self.statusIcon.connect('popup-menu', self.popup_menu_cb, self.menu)
         self.statusIcon.set_visible(1)
-    def komut1(self,w,data):
-	os.system("killall masgor  &")    
-	gtk.main_quit()
-    def tercihler(self,w,data):
-	import tercihler
-	ayar = tercihler.ayar()
-    def komut(self,w,data):
-    	if self.Item.get_active():
-    		os.system("./masgor  1 &")     
-	else:
-		os.system("killall masgor &")    
-    def komut2(self,w,data):
-        import hakkinda
-        hakkinda.hakkinda()   		
+
     def popup_menu_cb(self,w,x,time,data):
   #  self.popup_for_device(None, parent_menu_shell, parent_menu_item, func, data, button, activate_time)
-        
         data.popup(None, None, None, self.statusIcon, 3, time)
+        
 if __name__ == "__main__":
     area()
     gtk.main()
